0ea2642222cc8d0eb47eabfacc82b804f16cf87e
Li <liguohui01@baidu.com>
2018-07-16 15:29:31 +0800

    some eg.

--- config.js
+++ config.js
@@ -4,5 +4,9 @@ module.exports = {
 	localStaticDomain: '//localhost:8000',
 	publicStaticDomain: '//gh.bdstatic.com',
 	libs: [],// 需要额外打包的公共库
+	commons: false,// 是否需要提取公共文件（2次以上引入，仅限js）优先级低于libs
+	vendors: true,// 是否需要提取node_modules中的模块（包含css） 优先级低于commons
+	uglifyJs: true, // 开发模式下无效
+	optimizeCSS: true,// 开发模式下无效
 	supportFileType: ['less']// 需要支持的文件格式
 }
